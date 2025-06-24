5e81d6cb66c54c0388f6fd01dcb4577cba840ff1
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-25 02:08:59 +0300

    Implement speed fade for mobile tap movement

--- mobile-tap-move.js
+++ mobile-tap-move.js
@@ -1,9 +1,11 @@
 AFRAME.registerComponent('mobile-tap-move', {
     schema: {
-        speed: { type: 'number', default: 0.05 }
+        speed: { type: 'number', default: 0.05 },
+        fadeDuration: { type: 'number', default: 300 } // ms to reach target speed
     },
     init: function () {
         this.moving = false;
+        this.currentSpeed = 0;
         this.onTap = this.onTap.bind(this);
 
         if (AFRAME.utils.device.isMobile()) {
@@ -21,18 +23,34 @@ AFRAME.registerComponent('mobile-tap-move', {
         if (this.el.sceneEl.canvas) {
             this.el.sceneEl.canvas.removeEventListener('touchstart', this.onTap);
         }
+        this.moving = false;
+        this.currentSpeed = 0;
     },
     onTap: function (evt) {
         evt.preventDefault();
         this.moving = !this.moving;
     },
     tick: function (time, delta) {
-        if (!this.moving) return;
         const camera = this.el.sceneEl.camera;
         if (!camera) return;
+
+        const fadeDuration = this.data.fadeDuration;
+        const deltaSec = delta / 1000;
+        if (fadeDuration > 0) {
+            const accel = this.data.speed / (fadeDuration / 1000);
+            if (this.moving) {
+                this.currentSpeed = Math.min(this.currentSpeed + accel * deltaSec, this.data.speed);
+            } else {
+                this.currentSpeed = Math.max(this.currentSpeed - accel * deltaSec, 0);
+            }
+        } else {
+            this.currentSpeed = this.moving ? this.data.speed : 0;
+        }
+
+        if (this.currentSpeed === 0) return;
         const dir = new THREE.Vector3();
         camera.getWorldDirection(dir);
-        const d = this.data.speed * (delta / 1000);
+        const d = this.currentSpeed * deltaSec;
         dir.multiplyScalar(d);
         camera.el.object3D.position.add(dir);
     }
