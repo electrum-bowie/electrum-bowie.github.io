28a2fed5d7f0ed24b2b752a70d404129b232db29
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-25 15:09:22 +0300

    Enable WebXR hand tracking

--- two-hand-manipulation.js
+++ two-hand-manipulation.js
@@ -3,8 +3,8 @@ AFRAME.registerComponent('two-hand-manipulation', {
         const sceneEl = this.el.sceneEl;
         this.leftController = sceneEl.querySelector('[oculus-touch-controls][hand=left]');
         this.rightController = sceneEl.querySelector('[oculus-touch-controls][hand=right]');
-        this.leftHand = sceneEl.querySelector('[hand-controls][hand=left]');
-        this.rightHand = sceneEl.querySelector('[hand-controls][hand=right]');
+        this.leftHand = sceneEl.querySelector('[hand-controls][hand=left], [hand-tracking-controls][hand=left]');
+        this.rightHand = sceneEl.querySelector('[hand-controls][hand=right], [hand-tracking-controls][hand=right]');
         this.leftSource = null;
         this.rightSource = null;
         this.leftGripPressed = false;
