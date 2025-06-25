82c73b4b3236f795a3f0170ad0a774f45c576d54
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-25 23:09:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -295,15 +295,20 @@ AFRAME.registerComponent("gaussian_splatting", {
                 const sliderElement = document.getElementById("slider");
                 const sliderValueElement = document.getElementById("slider-value");
                 const sliderLabelElement = document.getElementById("slider-label");
-                let sliderValue =
-                        typeof window !== 'undefined' &&
-                        typeof window.latestSliderValue === 'number'
-                                ? window.latestSliderValue
-                                : 1;
+                let sliderValue = 1;
                 if (sliderElement) {
                         const min = parseFloat(sliderElement.min);
                         const max = parseFloat(sliderElement.max);
+                        
+                        sliderValue = parseFloat(sliderElement.value);
+                        
+                        window.latestSliderValue = sliderValue;
+                        
                         sliderValue = min + max - sliderValue;
+                }
+                else if (typeof window !== 'undefined' &&
+                        typeof window.latestSliderValue === 'number') {
+                        sliderValue = window.latestSliderValue;
                 }
 
                 vertexCount = vertexCount / (isNaN(sliderValue) ? 1 : sliderValue);
