2c5ee2a2bfcfec92258dd853e2dfe844d0f2f003
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-27 02:31:26 +0300

    Enable WebXR multiview

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ AFRAME.registerComponent("gaussian_splatting", {
                 this.needsQualityUpdate = false;
                 this.initGL(this.el.sceneEl.camera.el.components.camera.camera, this.el.object3D, this.el.sceneEl.renderer);
                 this.loadData(this.data.src);
+                this.el.sceneEl.renderer.xr.addEventListener("sessionstart", () => {
+                        const gl = this.el.sceneEl.renderer.getContext();
+                        const ext = gl.getExtension("OVR_multiview2") || gl.getExtension("OVR_multiview");
+                        if (ext && this.el.sceneEl.renderer.xr.setMultiviewEnabled) {
+                                this.el.sceneEl.renderer.xr.setMultiviewEnabled(true);
+                                console.log("Multiview enabled");
+                        } else {
+                                console.log("Multiview not supported");
+                        }
+                });
         },
 	// also works from vanilla three.js
 	initGL: function (camera, object, renderer) {
