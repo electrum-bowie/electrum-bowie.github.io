cf7a3325670974f373fb00073734a42fc31c5164
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-27 20:14:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                 const pixelRatio = this.data.pixelRatio < 0 ? window.devicePixelRatio : this.data.pixelRatio;
                 const xrPixelRatio = this.data.xrPixelRatio < 0 ? window.devicePixelRatio : this.data.xrPixelRatio;
                 this.el.sceneEl.renderer.setPixelRatio(pixelRatio);
-                // this.el.sceneEl.renderer.xr.setFramebufferScaleFactor(xrPixelRatio);
-		this.el.sceneEl.renderer.xr.setPixelRatio(xrPixelRatio);
+                this.el.sceneEl.renderer.xr.setFramebufferScaleFactor(xrPixelRatio);
                 this.originalBuffers = [];
                 this.needsQualityUpdate = false;
                 this.initGL(this.el.sceneEl.camera.el.components.camera.camera, this.el.object3D, this.el.sceneEl.renderer);
