477516101afa2adfd5ee6777812dd59c3782a0f4
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-29 01:25:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -273,11 +273,11 @@ AFRAME.registerComponent("gaussian_splatting", {
 							const mbps = (bytesDownloaded / 1024 / 1024) / ((Date.now() - start) / 1000);
 							const percent = bytesDownloaded / totalDownloadBytes * 100;
 							if (percent - lastReportedProgress > 1) {
-                                                        console.log("progress:", percent.toFixed(2) + "%", mbps.toFixed(2) + " Mbps");
+                                                        console.log("Progress:", percent.toFixed(2) + "%", mbps.toFixed(2) + " Mbps");
 								lastReportedProgress = percent;
 							}
 						} else {
-                                                console.log("progress:", bytesDownloaded, ", unknown total");
+                                                console.log("Progress:", bytesDownloaded, ", unknown total");
 						}
 						chunks.push(value);
 						if (!this.textureReady &&
