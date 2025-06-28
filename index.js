cff30dbcae6c5265ca1ce9df3783d6d34b183dac
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-28 16:18:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent("gaussian_splatting", {
         schema: {
                 src: { type: 'string', default: "" },
                 pixelRatio: { type: 'number', default: 0.5 },
-                xrPixelRatio: { type: 'number', default: 0.8 },
+                xrPixelRatio: { type: 'number', default: 0.7 },
                 foveation: { type: 'number', default: 3.0 },
         },
         init: function () {
