d98e88b5646b49ac5d77305a8703781d235c5b8b
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-28 21:49:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 
 				void main () {
 					float A = -dot(vPosition, vPosition);
-					if (A < -3.0) discard;
+					if (A < -3.45) discard;
 					float B = exp(A) * vColor.a;
 					gl_FragColor = vec4(vColor.rgb, B);
 				}
