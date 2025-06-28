93d04aa45513739d312c052603026e28f2becdda
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-28 20:59:26 +0300

    Update two-hand-manipulation.js

--- two-hand-manipulation.js
+++ two-hand-manipulation.js
@@ -187,7 +187,7 @@ AFRAME.registerComponent('two-hand-manipulation', {
 
         const currentVector = this._tmpVec3.copy(rightPos).sub(leftPos).normalize();
         const heightDiff = Math.abs(leftPos.y - rightPos.y);
-        const heightThresh = currentDistance * 0.1;
+        const heightThresh = currentDistance * 0.35;
         const newMode = heightDiff < heightThresh ? 'yaw' : 'axis';
 
         if (this.rotationMode !== newMode) {
