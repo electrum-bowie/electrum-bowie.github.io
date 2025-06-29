301666f12fb25f269f26403e7ee793d6de577228
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-29 18:04:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -640,7 +640,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 
                                 let depth = view[0] * px + view[1] * py + view[2] * pz + view[3];
 
-                                if (depth > -0.18) continue;
+                                if (depth > -0.22) continue;
 
                                 if (depth < 0 && matrices[i * 16 + 15] * scaleFactor > threshold * depth) {
                                         depthList[validCount] = depth;
