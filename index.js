792d934c83268be7fa14a13d90cb479278600c5d
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-06-29 18:46:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -431,7 +431,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 f_buffer[8 * i + 3 + 2]
                         );
                         const maxScale = 10.0;
-                        const minScale = 0.005;
+                        const minScale = 0.002;
                         if (Math.max(scale.x, scale.y, scale.z) > maxScale ||
                                 Math.max(scale.x, scale.y, scale.z) < minScale) {
                                 continue;
