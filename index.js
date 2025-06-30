35090119430321a933acb22a5bb6be7cfa6c17b3
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-01 01:10:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -675,7 +675,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                 let matrices = undefined;
 
                 const sortSplats = function sortSplats(matrices, view, mvp, scaleFactor = 1.0, sliderValue = 1, focal = 1.0) {
-                        const sizeThreshold = 0.00016 * (isNaN(sliderValue) ? 1 : sliderValue);
+                        const sizeThreshold = 0.00005 * (isNaN(sliderValue) ? 1 : sliderValue);
                         const vertexCount = matrices.length / 16;
                         let threshold = -0.001;
 
