30316f0291089ce2ff11a6481aa609a541392992
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-02 00:45:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -780,7 +780,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const edgeDist = Math.max(Math.abs(ndcX), Math.abs(ndcY));
                                 const edgeMultiplier = 1.0 + (edgeDist * 0.5);
                                 const pixelRadius = focal * radius / (-depth);
-                                if (pixelRadius < 1.0 * edgeMultiplier) continue;
+                                if ((pixelRadius < 1.0 * edgeMultiplier) && !skipCull) continue;
                                 
                                 if (matrices[i * 16 + 15] * scaleFactor > threshold * depth) {
                                         depthList[validCount] = depth;
