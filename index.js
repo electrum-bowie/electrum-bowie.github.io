534bdc00cde51526f93e9857b1ec766df53d24da
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-01 23:06:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -804,7 +804,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         for (let i = 0; i < validCount; i++) depthIndex[starts0[sizeList[i]]++] = validIndexList[i];
 
                         // Occlusion-based discarding
-                        const gridSize = 90;
+                        const gridSize = 256;
                         const coverage = new Float32Array(gridSize * gridSize);
                         let tmpVisible = new Uint32Array(validCount);
                         let visibleCount = 0;
