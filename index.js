7021e96baf5986f2119979da14ce3ddc2eb59b1e
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-01 21:37:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -502,7 +502,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 			covAndColorData_uint8[destOffset + 3] = u_buffer[32 * i + 24 + 3];
 
 			// Store scale and transparent to remove splat in sorting process
-			mtx.elements[15] = Math.max(scale.x, scale.y, scale.z);
+			mtx.elements[15] = Math.max(scale.x, scale.y, scale.z) * u_buffer[32 * i + 24 + 3] / 255.0;
                         mtx.elements[11] = u_buffer[32*i + 24 + 3] / 255.0;
 
 			for (let j = 0; j < 16; j++) {
@@ -747,7 +747,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                         continue;
                                 }
 
-                                const radius = matrices[i*16 + 15] * scaleFactor;
+                                const radius = matrices[i * 16 + 15] * scaleFactor;
                                 
                                 const skipCull = (radius / scaleFactor) > 1.0;
 
