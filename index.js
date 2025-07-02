a52c0839a9b6e53844b616d478cd1f6dd8e8d6e6
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-03 01:43:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -870,8 +870,8 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 }
 
                                 const isBig = false; // baseRadius > 0.05;
-                                const stillVisible = totalWeight - occludedWeight;
-                                if (isBig || totalWeight === 0.0 || stillVisible > 0.01) {
+                                const stillVisible = 1 - (occludedWeight / totalWeight);
+                                if (isBig || totalWeight === 0.0 || stillVisible > 0.0001) {
                                         tmpVisible[visibleCount++] = idx;
                                         for (let y = minY; y <= maxY; y++) {
                                                 for (let x = minX; x <= maxX; x++) {
