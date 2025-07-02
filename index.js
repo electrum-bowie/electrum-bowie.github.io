b61fb5827a3f95ae3e5769849e94e8be39ab3b3e
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-03 01:09:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -822,6 +822,11 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const clip_z = mvp[2] * px + mvp[6] * py + mvp[10] * pz + mvp[14];
                                 const clip_w = mvp[3] * px + mvp[7] * py + mvp[11] * pz + mvp[15];
 
+                                if (clip_w <= 0.0 || clip_z <= -clip_w) {
+                                        tmpVisible[visibleCount++] = idx;   // keep it in the draw list
+                                        continue;                           // but do NOT touch coverage[]
+                                }
+
                                 const opacity = matrices[idx * 16 + 11];
 
                                 const invW  = 1.0 / clip_w;
