63a0a8325b80fc015b6d505fb850e1f03b1af995
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-06 15:17:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -821,7 +821,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                 };
 
                 const occlusionSort = function occlusionSort(matrices, depthIndex, view, mvp, scaleFactor = 1.0) {
-                        const gridSize = 64;
+                        const gridSize = 128;
                         const coverage = new Float32Array(gridSize * gridSize);
                         let tmpVisible = new Uint32Array(depthIndex.length);
                         let visibleCount = 0;
@@ -870,7 +870,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const minY = Math.max(0, Math.floor(cy - r));
                                 const maxY = Math.min(gridSize - 1, Math.ceil(cy + r));
 
-                                const isBig = false // baseRadius > 0.1;
+                                const isBig = baseRadius > 0.1;
                                 
                                 const r2 = r * r;
                                 for (let y = minY; y <= maxY; y++) {
@@ -890,7 +890,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                         }
                                 }
                                 const stillVisible = 1 - (occludedWeight / totalWeight);
-                                if (isBig || totalWeight === 0.0 || stillVisible > 0.000001) {
+                                if (isBig || totalWeight === 0.0 || stillVisible > 0.0000001) {
                                         tmpVisible[visibleCount++] = idx;
                                         for (let y = minY; y <= maxY; y++) {
                                         for (let x = minX; x <= maxX; x++) {
