1beb0fb4b724d3afc70d89fa100c866465bcb43d
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-11 20:07:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -784,10 +784,10 @@ AFRAME.registerComponent("gaussian_splatting", {
 
                                 let totalWeight = 0.0, occludedWeight = 0.0;
                                 
-                                const minX = Math.max(0, Math.floor(cx - rX));
-                                const maxX = Math.min(gridSizeX - 1, Math.ceil(cx + rX));
-                                const minY = Math.max(0, Math.floor(cy - rY));
-                                const maxY = Math.min(gridSizeY - 1, Math.ceil(cy + rY));
+                                const minX = Math.max(0, Math.floor(cx) - 1);
+                                const maxX = Math.min(gridSizeX - 1, Math.floor(cx) + 1);
+                                const minY = Math.max(0, Math.floor(cy) - 1);
+                                const maxY = Math.min(gridSizeY - 1, Math.floor(cy) + 1);
 
                                 const isBig = false // baseRadius > 0.1;
                                 
