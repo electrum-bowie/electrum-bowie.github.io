656ea9b8d008546f850ff51644c6e8216529f88a
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-11 17:41:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -748,7 +748,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const clip_z = mvp[2] * px + mvp[6] * py + mvp[10] * pz + mvp[14];
                                 const clip_w = mvp[3] * px + mvp[7] * py + mvp[11] * pz + mvp[15];
 
-                                if (clip_w <= 0.0) {
+                                if (clip_w <= 0.0 || clip_z <= 0.0 || clip_z <= -clip_w) {
                                         tmpVisible[visibleCount++] = idx;
                                         continue;
                                 }
