9d9d7caa5722d9d6b3c6697cc38bb580f02525a6
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-13 01:31:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -776,15 +776,16 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 
                                 const ndcRadius = Math.abs(radius / depth);
                                 
-                                const cx = (ndcX * 0.5 + 0.5) * (gridSizeX - 1);
-                                const cy = (ndcY * 0.5 + 0.5) * (gridSizeY - 1);
-                                const rX = ndcRadius * (gridSizeX - 1) * 0.5;
-                                const rY = ndcRadius * (gridSizeY - 1) * 0.5;
-                                
-                                const minX = Math.max(0, Math.ceil(cx - rX));
-                                const maxX = Math.min(gridSizeX - 1, Math.floor(cx + rX));
-                                const minY = Math.max(0, Math.ceil(cy - rY));
-                                const maxY = Math.min(gridSizeY - 1, Math.floor(cy + rY));
+                                const cx = (ndcX * 0.5 + 0.5) * gridSizeX;
+                                const cy = (ndcY * 0.5 + 0.5) * gridSizeY;
+                                const rX = ndcRadius * gridSizeX * 0.5;
+                                const rY = ndcRadius * gridSizeY * 0.5;
+
+                                let totalWeight = 0.0, occludedWeight = 0.0;
+                                const minX = Math.max(0, Math.floor(cx - r));
+                                const maxX = Math.min(gridSize - 1, Math.ceil(cx + r));
+                                const minY = Math.max(0, Math.floor(cy - r));
+                                const maxY = Math.min(gridSize - 1, Math.ceil(cy + r));
 
                                 const opacity = matrices[idx * 16 + 11];
                                 const alphaContrib = opacity ** 4;
