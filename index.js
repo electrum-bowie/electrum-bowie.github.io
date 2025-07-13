f8290d529b8d5bc631a363f9c4c72bf4e1300956
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-14 01:32:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -108,9 +108,12 @@ AFRAME.registerComponent("gaussian_splatting", {
 		const positionsArray = new Float32Array(6 * 3);
 		const positions = new THREE.BufferAttribute(positionsArray, 3);
 		baseGeometry.setAttribute('position', positions);
-		positions.setXYZ(0, -2.0, -2.0, 0.0);
-                positions.setXYZ(1, 2.0, -2.0, 0.0);
-                positions.setXYZ(2, 0.0,  2.0, 0.0);
+		positions.setXYZ(2, -1.0, 1.0, 0.0);
+		positions.setXYZ(1, 1.0, 1.0, 0.0);
+		positions.setXYZ(0, -1.0, -1.0, 0.0);
+		positions.setXYZ(5, -1.0, -1.0, 0.0);
+		positions.setXYZ(4, 1.0, 1.0, 0.0);
+		positions.setXYZ(3, 1.0, -1.0, 0.0);
                 positions.needsUpdate = true;
 
 		const geometry = new THREE.InstancedBufferGeometry().copy(baseGeometry);
