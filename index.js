65ee29c86dad0809d5e2d4aeaf0d3c373e1e95f2
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-13 13:45:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -819,7 +819,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 							const cellCenterX = ((x + 0.5) * 2.0 / gridSizeX) - 1.0;
 							const dx2 = (cellCenterX - ndcX) ** 2;
 
-							if (dx2 + dy2 > ndcRadius2) continue;
+							if (dx2 + dy2 > ndcRadius2 * 0.5) continue;
 
 							const idx2 = y * gridSizeX + x;
                                                         coverage[idx2] = Math.min(1.0, coverage[idx2] + alphaContrib);
