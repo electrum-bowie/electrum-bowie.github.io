7c0c29fe0242d5862fc6fe7eea469d7599c57c7a
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-21 23:58:15 +0300

    Update two-hand-manipulation.js

--- two-hand-manipulation.js
+++ two-hand-manipulation.js
@@ -173,7 +173,7 @@ AFRAME.registerComponent('two-hand-manipulation', {
         const now = performance.now();
 
         if (this.delayScaleRotate) {
-            if (now - this.twoHandStartTime < 100) {
+            if (now - this.twoHandStartTime < 110) {
                 const newWorldPos = midpoint.clone().add(this.startOffset);
                 if (this.el.object3D.parent) this.el.object3D.parent.worldToLocal(newWorldPos);
                 this.el.object3D.position.copy(newWorldPos);
