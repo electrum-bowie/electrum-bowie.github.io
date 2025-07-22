ab33f138735f4349220e6d3b3a87e7022091c256
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-07-22 11:23:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 			`,
 			blending: THREE.CustomBlending,
 			blendSrcAlpha: THREE.OneFactor,
-			depthTest: false,
+			depthTest: true,
         		depthWrite: false,
                         transparent: true
                 });
@@ -745,7 +745,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const edgeDist = Math.max(Math.abs(ndcX), Math.abs(ndcY));
                                 const edgeMultiplier = 1.0 + (edgeDist * 0.4);
                                 const pixelRadius = focal * radiusTransparencyProduct / (-depth);
-                                if ((pixelRadius < 0.65 * edgeMultiplier) && !skipCull) continue;
+                                if ((pixelRadius < 0.85 * edgeMultiplier) && !skipCull) continue;
                                 
                                 if (radius > threshold * depth) {
                                         depthList[validCount] = depth;
@@ -771,7 +771,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         // Occlusion-based discarding
                         const cellW = 2.0 / gridSizeX;
                         const cellH = 2.0 / gridSizeY;
-                        const cellDiag2 = (cellW * cellW + cellH * cellH) * 0.5;
+                        const cellDiag2 = (cellW * cellW + cellH * cellH) * 0.0;
 
                         coverage.fill(0);
                         let tmpVisible = cache.tmpVisible;
