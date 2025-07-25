0233f6bb71cfb0195587af354fd4909d212f82d8
Stoian Andre <theendergaming26@gmail.com>
2025-07-25 14:11:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -734,7 +734,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 }
                                 
                                 const edgeDist = Math.max(Math.abs(ndcX), Math.abs(ndcY));
-                                const edgeMultiplier = 1.0 + (edgeDist * 0.4);
+                                const edgeMultiplier = 1.0 + (edgeDist * 0.5);
                                 const pixelRadius = (focal * radiusTransparencyProduct) / -depth;
                                 if ((pixelRadius < 1.0 * edgeMultiplier) && !skipCull) continue;
                                 
