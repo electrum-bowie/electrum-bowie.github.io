2dc06015dec7d09acae8f9dd6db158c52f9e634f
Stoian Andre <theendergaming26@gmail.com>
2025-07-31 17:50:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -721,7 +721,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         const vertexCount = matrices.length / 16;
                         if (!fadeOpacities || fadeOpacities.length < vertexCount) {
                                 const tmp = new Float32Array(vertexCount);
-                                tmp.fill(2.0);
+                                tmp.fill(-1.0);
                                 if (fadeOpacities) tmp.set(fadeOpacities.subarray(0, Math.min(fadeOpacities.length, vertexCount)));
                                 fadeOpacities = tmp;
                         }
@@ -791,10 +791,10 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 let f = fadeOpacities[i];
 
                                 if (tooSmall) {
-                                        if (f === 2.0) f = 0.0; // default unset value is 2.0
+                                        if (f === -1.0) f = 0.0; // default unset value is -1.0
                                         f = Math.max(0, f - fadeStep);
                                 } else {
-                                        if (f === 2.0) f = 1.0; // default unset value is 2.0
+                                        if (f === -1.0) f = 1.0; // default unset value is -1.0
                                         f = Math.min(1, f + fadeStep);
                                 }
                                 fadeOpacities[i] = f;
@@ -860,7 +860,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         if (e.data.method == "push") {
                                 new_matrices = new Float32Array(e.data.matrices);
                                 const newFade = new Float32Array(new_matrices.length / 16);
-                                newFade.fill(2.0);
+                                newFade.fill(-1.0);
                                 if (matrices === undefined) {
                                         matrices = new_matrices;
                                         fadeOpacities = newFade;
@@ -891,7 +891,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 if (matrices === undefined) {
                                         const sortedIndexes = new Uint32Array(1);
                                         const fadeCopy = new Float32Array(1);
-                                        fadeCopy[0] = 2.0;
+                                        fadeCopy[0] = -1.0;
                                         self.postMessage({ method: "sort", sortedIndexes, fadeOpacities: fadeCopy }, [sortedIndexes.buffer, fadeCopy.buffer]);
                                 } else {
                                         const sortedIndexes = sortSplats();
