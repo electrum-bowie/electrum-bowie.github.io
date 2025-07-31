62a212f2c7d518db3975a71c39c3f264f58165c8
Stoian Andre <theendergaming26@gmail.com>
2025-08-01 00:03:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -694,7 +694,6 @@ AFRAME.registerComponent("gaussian_splatting", {
                         sizeList: null,
                         validIndexList: null,
                         depthIndex: null,
-                        tmpVisible: null,
                 };
 
                 const counts0 = new Uint32Array(COUNT_SIZE);
@@ -708,7 +707,6 @@ AFRAME.registerComponent("gaussian_splatting", {
                         cache.sizeList = new Int32Array(cache.depthList.buffer);
                         cache.validIndexList = new Int32Array(n);
                         cache.depthIndex = new Uint32Array(n);
-                        cache.tmpVisible = new Uint32Array(n);
                 };
 
                 const filterSplats = function filterSplats(matrices, view, mvp, scaleFactor = 1.0, focal = 1.0) {
@@ -735,7 +733,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         const m8 = mvp[8],  m9 = mvp[9],  m10 = mvp[10], m11 = mvp[11];
                         const m12 = mvp[12], m13 = mvp[13], m14 = mvp[14], m15 = mvp[15];
 
-                        const fadeStep = 0.2;
+                        const fadeStep = 0.3;
                         const nearPlaneClip = -0.16;
                         for (let offset = 0, i = 0; i < vertexCount; offset += 16, i++) {
                                 const px = matrices[offset + 12];
@@ -746,12 +744,16 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const clip_y = m1 * px + m5 * py + m9  * pz + m13;
                                 const clip_z = m2 * px + m6 * py + m10 * pz + m14;
                                 const clip_w = m3 * px + m7 * py + m11 * pz + m15;
+                                
+				if (clip_w <= 0.0 || clip_z <= -clip_w) {
+                                        continue;
+                                }
 
                                 const radius = matrices[offset + 15] * scaleFactor;
                                 const transparency = matrices[offset + 11]; // 0-1
                                 const radiusTransparencyProduct = radius * transparency;
                                 
-                                const skipCull = (radiusTransparencyProduct / scaleFactor) > 0.1;
+                                const skipCull = (radiusTransparencyProduct / scaleFactor) > 0.01;
                                 
                                 const invW  = 1.0 / clip_w;
 
@@ -835,18 +837,8 @@ AFRAME.registerComponent("gaussian_splatting", {
                         let depthIndex = cache.depthIndex;
                         for (let i = 0; i < validCount; i++) depthIndex[starts0[sizeList[i]]++] = validIndexList[i];
 
-			let tmpVisible = cache.tmpVisible;
-                        let visibleCount = 0;
-
-                        for (let j = validCount - 1; j >= 0; j--) {
-                                const idx = depthIndex[j];
-                                tmpVisible[visibleCount++] = idx;
-                        }
-
-                        let result = new Uint32Array(visibleCount);
-                        for (let i = 0, j = visibleCount - 1; i < visibleCount; i++, j--) {
-                                result[i] = tmpVisible[j];
-                        }
+                        const result = new Uint32Array(validCount);
+                        result.set(depthIndex.subarray(0, validCount));
 
                         return result;
                 };
