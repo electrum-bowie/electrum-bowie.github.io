c93c123d461836747970da38e9e893d88e7b813b
Stoian Andre <theendergaming26@gmail.com>
2025-08-01 23:37:23 +0300

    Revert "Merge pull request #276 from electrum-bowie/codex/optimize-fade-management-for-main-thread"
    This reverts commit d007fce4fc3fc15f4dea46307e3f4f40aa16f5e5, reversing
    changes made to b92f4a1c9c6e3bcc1cd2298d39b981f8754fee79.

--- index.js
+++ index.js
@@ -100,7 +100,8 @@ AFRAME.registerComponent("gaussian_splatting", {
                 this.covAndColorTexture.internalFormat = "RGBA32UI";
                 this.covAndColorTexture.needsUpdate = true;
 
-                this.fadeOpacityTexture = new THREE.DataTexture(new Uint8Array(4096 * 4096), 4096, 4096, THREE.RedFormat, THREE.UnsignedByteType);
+                this.fadeOpacityData = new Uint8Array(4096 * 4096);
+                this.fadeOpacityTexture = new THREE.DataTexture(this.fadeOpacityData, 4096, 4096, THREE.RedFormat, THREE.UnsignedByteType);
                 this.fadeOpacityTexture.generateMipmaps = false;
                 this.fadeOpacityTexture.minFilter = THREE.NearestFilter;
                 this.fadeOpacityTexture.magFilter = THREE.NearestFilter;
@@ -295,7 +296,8 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 this.filterReady = true;
                         }
                         if (e.data.fadeOpacities) {
-                                this.fadeOpacityTexture.image.data = new Uint8Array(e.data.fadeOpacities);
+                                const fades = new Uint8Array(e.data.fadeOpacities);
+                                this.fadeOpacityData.set(fades);
                                 this.fadeOpacityTexture.needsUpdate = true;
                         }
                 };
@@ -476,6 +478,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 			covAndColorData_uint8[destOffset + 2] = u_buffer[32 * i + 24 + 2];
                         covAndColorData_uint8[destOffset + 3] = u_buffer[32 * i + 24 + 3];
 
+                        this.fadeOpacityData[this.loadedVertexCount + i] = 255;
 
 			// Store scale and transparent to remove splat in sorting process
 			mtx.elements[15] = Math.max(scale.x, scale.y, scale.z);
@@ -513,9 +516,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 
                         const fadeOpacityTextureProperties = this.renderer.properties.get(this.fadeOpacityTexture);
                         gl.bindTexture(gl.TEXTURE_2D, fadeOpacityTextureProperties.__webglTexture);
-                        const fadeChunk = new Uint8Array(width * height);
-                        fadeChunk.fill(255);
-                        gl.texSubImage2D(gl.TEXTURE_2D, 0, xoffset, yoffset, width, height, gl.RED, gl.UNSIGNED_BYTE, fadeChunk);
+                        gl.texSubImage2D(gl.TEXTURE_2D, 0, xoffset, yoffset, width, height, gl.RED, gl.UNSIGNED_BYTE, this.fadeOpacityData, this.loadedVertexCount);
 
 			this.loadedVertexCount += width * height;
 			vertexCount -= width * height;
