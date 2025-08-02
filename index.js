06b64acba3761964314117b0e33d30ab081201dc
Stoian Andre <theendergaming26@gmail.com>
2025-08-02 16:20:29 +0300

    Revert "Merge pull request #289 from electrum-bowie/tbsiwt-codex/optimize-splat-fade-transparency-handling"
    This reverts commit 21e5afc957e9ff79c7d7a4d9f0d83b4b51207407, reversing
    changes made to cdc61f2cdbe5cf0cd8504a85bcc2b5ee362abaa5.

--- index.js
+++ index.js
@@ -297,12 +297,13 @@ AFRAME.registerComponent("gaussian_splatting", {
                         }
                         if (e.data.fadeOpacities) {
                                 const fades = new Uint8Array(e.data.fadeOpacities);
-                                this.updateFadeTexture(fades);
+                                this.fadeOpacityData.set(fades);
+                                this.fadeOpacityTexture.needsUpdate = true;
                         }
                 };
                 this.sortReady = true;
                 this.filterReady = true;
-        },
+	},
         loadData: function (src) {
                 this.loadedVertexCount = 0;
                 this.rowLength = 3 * 4 + 3 * 4 + 4 + 4;
@@ -521,38 +522,11 @@ AFRAME.registerComponent("gaussian_splatting", {
 			vertexCount -= width * height;
 		}
 
-                this.worker.postMessage({
-                        method: "push",
-                        matrices: matrices.buffer
-                }, [matrices.buffer]);
-        },
-        updateFadeTexture: function (fades) {
-                const gl = this.renderer.getContext();
-                const fadeOpacityTextureProperties = this.renderer.properties.get(this.fadeOpacityTexture);
-                gl.bindTexture(gl.TEXTURE_2D, fadeOpacityTextureProperties.__webglTexture);
-
-                const rowWidth = 4096;
-                const fullRows = Math.floor(fades.length / rowWidth);
-                if (fullRows > 0) {
-                        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, rowWidth, fullRows, gl.RED, gl.UNSIGNED_BYTE, fades, 0);
-                }
-
-                const remaining = fades.length % rowWidth;
-                if (remaining > 0) {
-                        gl.texSubImage2D(
-                                gl.TEXTURE_2D,
-                                0,
-                                0,
-                                fullRows,
-                                remaining,
-                                1,
-                                gl.RED,
-                                gl.UNSIGNED_BYTE,
-                                fades,
-                                fullRows * rowWidth
-                        );
-                }
-        },
+		this.worker.postMessage({
+			method: "push",
+			matrices: matrices.buffer
+		}, [matrices.buffer]);
+	},
         tick: function (time, timeDelta) {
                 this.camera.getWorldPosition(this.tmpCameraPos);
                 
