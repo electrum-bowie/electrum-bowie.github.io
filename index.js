7e6f936adf87820ee1fdc1878294eaed9a3bfb89
Stoian Andre <theendergaming26@gmail.com>
2025-08-02 15:28:18 +0300

    Revert "Merge pull request #281 from electrum-bowie/zepfbi-codex/optimize-splat-fade-transparency-handling"
    This reverts commit 478c6fb4d01f4b3488f354c1298e10cbae565075, reversing
    changes made to 381930922bf8ca50c93ca14b3d90b85117d00a66.

--- index.js
+++ index.js
@@ -297,18 +297,8 @@ AFRAME.registerComponent("gaussian_splatting", {
                         }
                         if (e.data.fadeOpacities) {
                                 const fades = new Uint8Array(e.data.fadeOpacities);
-                                const gl = this.renderer.getContext();
-                                const texProps = this.renderer.properties.get(this.fadeOpacityTexture);
-                                gl.bindTexture(gl.TEXTURE_2D, texProps.__webglTexture);
-                                const total = fades.length;
-                                let offset = 0;
-                                let y = 0;
-                                while (offset < total) {
-                                        const width = Math.min(4096, total - offset);
-                                        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, y, width, 1, gl.RED, gl.UNSIGNED_BYTE, fades, offset);
-                                        offset += width;
-                                        y++;
-                                }
+                                this.fadeOpacityData.set(fades);
+                                this.fadeOpacityTexture.needsUpdate = true;
                         }
                 };
                 this.sortReady = true;
@@ -481,15 +471,17 @@ AFRAME.registerComponent("gaussian_splatting", {
 				covAndColorData_int16[destOffset + j] = parseInt(mtx.elements[cov_indexes[j]] * 32767.0 / max_value);
 			}
 
-                        // RGBA
-                        destOffset = this.loadedVertexCount * 16 + (i * 4 + 3) * 4;
-                        covAndColorData_uint8[destOffset + 0] = u_buffer[32 * i + 24 + 0];
-                        covAndColorData_uint8[destOffset + 1] = u_buffer[32 * i + 24 + 1];
-                        covAndColorData_uint8[destOffset + 2] = u_buffer[32 * i + 24 + 2];
+			// RGBA
+			destOffset = this.loadedVertexCount * 16 + (i * 4 + 3) * 4;
+			covAndColorData_uint8[destOffset + 0] = u_buffer[32 * i + 24 + 0];
+			covAndColorData_uint8[destOffset + 1] = u_buffer[32 * i + 24 + 1];
+			covAndColorData_uint8[destOffset + 2] = u_buffer[32 * i + 24 + 2];
                         covAndColorData_uint8[destOffset + 3] = u_buffer[32 * i + 24 + 3];
 
-                        // Store scale and transparent to remove splat in sorting process
-                        mtx.elements[15] = Math.max(scale.x, scale.y, scale.z);
+                        this.fadeOpacityData[this.loadedVertexCount + i] = 255;
+
+			// Store scale and transparent to remove splat in sorting process
+			mtx.elements[15] = Math.max(scale.x, scale.y, scale.z);
                         mtx.elements[11] = u_buffer[32*i + 24 + 3] / 255.0;
 
 			for (let j = 0; j < 16; j++) {
@@ -497,9 +489,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 			}
 		}
 
-                this.fadeOpacityData.fill(255, this.loadedVertexCount, this.loadedVertexCount + vertexCount);
-
-                const gl = this.renderer.getContext();
+		const gl = this.renderer.getContext();
 		while (vertexCount > 0) {
 			let width = 0;
 			let height = 0;
