ca01cab95f217437986a541b1c9e604317205f30
Stoian Andre <theendergaming26@gmail.com>
2025-08-02 17:06:29 +0300

    Revert "Merge pull request #294 from electrum-bowie/7irubh-codex/optimize-splat-fade-transparency-handling"
    This reverts commit 2ba8a7896bd74043f41251e0e4888fad007ac0c5, reversing
    changes made to 36c4bba6b91f06946bdc48d3a6641c53fd1025c4.

--- index.js
+++ index.js
@@ -297,12 +297,13 @@ AFRAME.registerComponent("gaussian_splatting", {
                         }
                         if (e.data.fadeOpacities) {
                                 const fades = new Uint8Array(e.data.fadeOpacities);
-                                this.updateFadeTexture(fades);
+                                this.fadeOpacityData.set(fades);
+                                this.fadeOpacityTexture.needsUpdate = true;
                         }
                 };
                 this.sortReady = true;
                 this.filterReady = true;
-        },
+	},
         loadData: function (src) {
                 this.loadedVertexCount = 0;
                 this.rowLength = 3 * 4 + 3 * 4 + 4 + 4;
@@ -521,27 +522,11 @@ AFRAME.registerComponent("gaussian_splatting", {
 			vertexCount -= width * height;
 		}
 
-                this.worker.postMessage({
-                        method: "push",
-                        matrices: matrices.buffer
-                }, [matrices.buffer]);
-        },
-        updateFadeTexture: function (fades) {
-                const gl = this.renderer.getContext();
-                const texProps = this.renderer.properties.get(this.fadeOpacityTexture);
-                gl.bindTexture(gl.TEXTURE_2D, texProps.__webglTexture);
-                let remaining = fades.length;
-                let srcOffset = 0;
-                let yoffset = 0;
-                while (remaining > 0) {
-                        const width = Math.min(4096, remaining);
-                        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, yoffset, width, 1, gl.RED, gl.UNSIGNED_BYTE, fades, srcOffset);
-                        remaining -= width;
-                        srcOffset += width;
-                        yoffset++;
-                }
-                this.fadeOpacityData = fades;
-        },
+		this.worker.postMessage({
+			method: "push",
+			matrices: matrices.buffer
+		}, [matrices.buffer]);
+	},
         tick: function (time, timeDelta) {
                 this.camera.getWorldPosition(this.tmpCameraPos);
                 
@@ -575,7 +560,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                 this.worker.postMessage({ method: "clear" });
                 this.centerAndScaleTexture.needsUpdate = true;
                 this.covAndColorTexture.needsUpdate = true;
-                this.fadeOpacityData = new Uint8Array(4096 * 4096);
+                this.fadeOpacityTexture.needsUpdate = true;
                 for (const buf of this.originalBuffers) {
                         this.pushDataBuffer(buf.slice(0), buf.byteLength / this.rowLength);
                 }
