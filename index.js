e1530181bdbb1d0c90a0a29a5520380a6276edb6
Stoian Andre <theendergaming26@gmail.com>
2025-08-03 17:07:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -738,7 +738,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         const m8 = mvp[8],  m9 = mvp[9],  m10 = mvp[10], m11 = mvp[11];
                         const m12 = mvp[12], m13 = mvp[13], m14 = mvp[14], m15 = mvp[15];
 
-                        const fadeStep = 0.3;
+                        const fadeStep = 0.25;
                         const nearPlaneClip = -0.08;
                         for (let offset = 0, i = 0; i < vertexCount; offset += 16, i++) {
                                 const px = matrices[offset + 12];
