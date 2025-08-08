de8e84022a04aff0ad429940abb1fa7a1960118c
Stoian Andre <theendergaming26@gmail.com>
2025-08-08 18:13:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -772,7 +772,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                         ensureCapacity(vertexCount);
 
                         const now = performance.now();
-                        const deltaTime = Math.min((now - lastFadeTime) / 1000, 0.4);
+                        const deltaTime = Math.min((now - lastFadeTime) / 1000, 0.3);
                         lastFadeTime = now;
 
                         const fadeSpeed = 1.5;
