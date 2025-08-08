a642b03f1ce9508be8aacbe44fac987f42210e1a
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-08-09 01:16:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1057,7 +1057,9 @@ AFRAME.registerComponent("gaussian_splatting", {
 				const insideOfScreen = ndcX >= -1.0 && ndcX <= 1.0 && ndcY >= -1.0 && ndcY <= 1.0;
 				if (!insideOfScreen) continue;
 
-                                const radius = scaleFactor * Math.sqrt(maxRadius * minRadius) * 1.15;
+                                const thinness = (maxRadius * minRadius) * 1.0;
+
+                                const radius = scaleFactor * thinness;
                                 const opacity = matrices[offset + 11]; // 0-1 (0 transparent, 1 opaque)
 
                                 const ndcRadius = radius / -depth;
