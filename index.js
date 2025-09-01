2899dee6af85e89cc02319214b33b21914e65df9
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-09-02 00:32:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("gaussian_splatting", {
         schema: {
                 src: { type: 'string', default: "" },
-                pixelRatio: { type: 'number', default: 1.0 },
+                pixelRatio: { type: 'number', default: 0.8 },
                 xrPixelRatio: { type: 'number', default: 1.0 },
                 // Fixed foveation level. Set to 0 to disable foveated rendering
                 foveation: { type: 'number', default: 0.0 }, // no perceived performance gains even with the maximum level
