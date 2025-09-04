9bc54dfc41b67fc9019394f32674c81e0054eff8
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-09-04 16:46:36 +0300

    ...

--- index.js
+++ index.js
@@ -1182,7 +1182,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 				const avgResidual = residual / cells;
                                                                 
                                 const perceived = opacity * avgResidual;
-				if (perceived < 0.00001) {
+				if (perceived < 0.0000001) {
     					discarded[discardCount++] = idx;
 				}
 
