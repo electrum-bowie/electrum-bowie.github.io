73de67de5d012af95a9705b5ba080dc03788d8de
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2025-09-05 15:04:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1176,10 +1176,10 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const gridRadiusX = ndcRadiusX * (GRID_SIZE * 0.5);
                                 const gridRadiusY = ndcRadiusY * (GRID_SIZE * 0.5);
 
-                                const x0 = Math.max(0, Math.floor(gridX - gridRadiusX));
-                                const y0 = Math.max(0, Math.floor(gridY - gridRadiusY));
-                                const x1 = Math.min(GRID_SIZE - 1, Math.ceil(gridX + gridRadiusX));
-                                const y1 = Math.min(GRID_SIZE - 1, Math.ceil(gridY + gridRadiusY));
+                                const x0 = Math.max(0, Math.round(gridX - gridRadiusX));
+                                const y0 = Math.max(0, Math.round(gridY - gridRadiusY));
+                                const x1 = Math.min(GRID_SIZE - 1, Math.round(gridX + gridRadiusX));
+                                const y1 = Math.min(GRID_SIZE - 1, Math.round(gridY + gridRadiusY));
                                 if (x1 < 0 || x0 >= GRID_SIZE || y1 < 0 || y0 >= GRID_SIZE) continue;
 
                                 let residual = 0.0;
