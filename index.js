f4e28af90909ab7baad10aa51e29948caf3abcb4
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2026-02-07 00:23:22 +0200

    Revert "Merge pull request #399 from electrum-bowie/codex/investigate-large-.ply-scene-import-issues-nsjqm1  m"
    This reverts commit 498de03942044cc5fdddc21a5bf4c66eb605112d, reversing
    changes made to 035a31f052c8316ab45806418243165394d61f22.

--- index.js
+++ index.js
@@ -1515,47 +1515,18 @@ AFRAME.registerComponent("gaussian_splatting", {
                         }
                 };
         },
-       findPlyHeaderEnd: function (ubuf) {
-               const endHeader = [101, 110, 100, 95, 104, 101, 97, 100, 101, 114, 10];
-               const endHeaderCrLf = [101, 110, 100, 95, 104, 101, 97, 100, 101, 114, 13, 10];
-               for (let i = 0; i < ubuf.length; i++) {
-                       if (ubuf[i] !== 101) {
-                               continue;
-                       }
-                       if (i + endHeader.length <= ubuf.length) {
-                               let match = true;
-                               for (let j = 1; j < endHeader.length; j++) {
-                                       if (ubuf[i + j] !== endHeader[j]) {
-                                               match = false;
-                                               break;
-                                       }
-                               }
-                               if (match) {
-                                       return { headerEndIndex: i, headerEndLength: endHeader.length };
-                               }
-                       }
-                       if (i + endHeaderCrLf.length <= ubuf.length) {
-                               let match = true;
-                               for (let j = 1; j < endHeaderCrLf.length; j++) {
-                                       if (ubuf[i + j] !== endHeaderCrLf[j]) {
-                                               match = false;
-                                               break;
-                                       }
-                               }
-                               if (match) {
-                                       return { headerEndIndex: i, headerEndLength: endHeaderCrLf.length };
-                               }
-                       }
-               }
-               return null;
-       },
        parsePlyHeader: function (inputBuffer) {
                const ubuf = new Uint8Array(inputBuffer);
-               const headerInfo = this.findPlyHeaderEnd(ubuf);
-               if (!headerInfo) {
+               const header = new TextDecoder().decode(ubuf.slice(0, 1024 * 10));
+               let header_end = "end_header\n";
+               let header_end_index = header.indexOf(header_end);
+               if (header_end_index < 0) {
+                       header_end = "end_header\r\n";
+                       header_end_index = header.indexOf(header_end);
+               }
+               if (header_end_index < 0) {
                        return null;
                }
-               const header = new TextDecoder().decode(ubuf.slice(0, headerInfo.headerEndIndex));
                const formatMatch = /format (ascii|binary_little_endian) 1\.0/.exec(header);
                const format = formatMatch ? formatMatch[1] : "binary_little_endian";
                const vertexMatch = /element vertex (\d+)/.exec(header);
@@ -1574,7 +1545,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                        char: { method: "getInt8", size: 1 },
                };
                for (let prop of header
-                       .slice(0, headerInfo.headerEndIndex)
+                       .slice(0, header_end_index)
                        .split(/\r?\n/)
                        .filter((k) => k.startsWith("property "))) {
                        const [, type, name] = prop.split(" ");
@@ -1589,7 +1560,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                        rowOffset: row_offset,
                        offsets,
                        types,
-                       headerByteLength: headerInfo.headerEndIndex + headerInfo.headerEndLength,
+                       headerByteLength: header_end_index + header_end.length,
                };
        },
        buildPlyBinaryBatch: function (plyState, pending, rowCount) {
@@ -1690,11 +1661,16 @@ AFRAME.registerComponent("gaussian_splatting", {
        },
        processPlyBuffer: function (inputBuffer) {
                const ubuf = new Uint8Array(inputBuffer);
-               const headerInfo = this.findPlyHeaderEnd(ubuf);
-               if (!headerInfo) {
-                       throw new Error("Unable to read .ply file header");
+               // 10KB ought to be enough for a header...
+               const header = new TextDecoder().decode(ubuf.slice(0, 1024 * 10));
+               let header_end = "end_header\n";
+               let header_end_index = header.indexOf(header_end);
+               if (header_end_index < 0) {
+                       header_end = "end_header\r\n";
+                       header_end_index = header.indexOf(header_end);
                }
-               const header = new TextDecoder().decode(ubuf.slice(0, headerInfo.headerEndIndex));
+               if (header_end_index < 0)
+                       throw new Error("Unable to read .ply file header");
                const formatMatch = /format (ascii|binary_little_endian) 1\.0/.exec(header);
                const format = formatMatch ? formatMatch[1] : "binary_little_endian";
                let vertexCount = parseInt(/element vertex (\d+)/.exec(header)[1]);
@@ -1712,7 +1688,7 @@ AFRAME.registerComponent("gaussian_splatting", {
                        uchar: "getUint8",
                };
                for (let prop of header
-                       .slice(0, headerInfo.headerEndIndex)
+                       .slice(0, header_end_index)
                        .split(/\r?\n/)
                        .filter((k) => k.startsWith("property "))) {
                        const [p, type, name] = prop.split(" ");
@@ -1727,7 +1703,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 
                if (format === "ascii") {
                        const bodyStr = new TextDecoder().decode(
-                               ubuf.slice(headerInfo.headerEndIndex + headerInfo.headerEndLength),
+                               ubuf.slice(header_end_index + header_end.length),
                        );
                        const lines = bodyStr.split(/\r?\n/);
                        const vertices = [];
