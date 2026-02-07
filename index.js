72b9cd93b3e713f3e8513c2665ffa3c9bcbc0b21
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2026-02-07 02:25:33 +0200

    Revert "Fix occlusion facing factor for splats"
    This reverts commit fd74dbcc2953fe09adc82d627f1430b6e059431c.

--- index.js
+++ index.js
@@ -1515,9 +1515,9 @@ AFRAME.registerComponent("gaussian_splatting", {
                                         const toCameraZ = cameraZ - pz;
                                         const distSq = toCameraX * toCameraX + toCameraY * toCameraY + toCameraZ * toCameraZ;
                                         if (distSq > 1.0) {
-                                        	const invLen = 1.0 / Math.sqrt(distSq);
+                                        	const invLen = Math.sqrt(distSq);
                                         	const dot = nx * toCameraX * invLen + ny * toCameraY * invLen + nz * toCameraZ * invLen;
-                                        	facingFactor = Math.max(0.0, dot);
+                                        	facingFactor = 1.0 - Math.max(0.0, dot);
 					}
                                 }
 
