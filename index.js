a2b53e434ba80a2d411e0c271e15ed02725bae7d
Stoian Andre <88616437+electrum-bowie@users.noreply.github.com>
2026-02-08 00:53:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1189,8 +1189,8 @@ AFRAME.registerComponent("gaussian_splatting", {
                                 const transparency = matrices[offset + 11]; // 0-1
                                 const radiusTransparencyProduct = radius * transparency;
                                 
-                                const skipCullEdges = (radiusTransparencyProduct / scaleFactor) > 0.075;
-				const skipCullBehind = (radiusTransparencyProduct / scaleFactor) > 0.3;
+                                const skipCullEdges = (radiusTransparencyProduct / scaleFactor) > 0.1;
+				const skipCullBehind = (radiusTransparencyProduct / scaleFactor) > 0.35;
 
                                 if (clip_w < 0.0 && !skipCullBehind) continue;
 
